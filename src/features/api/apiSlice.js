67619bd512ee72d9d42a5a8114061d293b7927a1
maja-98 <majajouhar@gmail.com>
2023-02-19 19:25:24 +0530

    updated Readme

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,8 +1,9 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-
+const env = (process.env.REACT_APP_ENVIRONMENT)
+const baseUrl = env==='PROD' ? 'https://dubai-pardha-palace.onrender.com/api': "http://localhost:3500/api"
 export const apiSlice  = createApi({
     reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:3500/api"}),
+    baseQuery:fetchBaseQuery({baseUrl}),
     endpoints: builder => ({})
 })
 
